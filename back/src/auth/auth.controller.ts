385e4305bc8d66ba4d944e1bab50ceee5e91b537
cerfio <yannis.cerfio@gmail.com>
2022-11-24 15:25:06 +0100

    fix login

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -4,12 +4,11 @@ import { ISignUpInput } from './dtos/auth.signup.input';
 import { AuthService } from './auth.service';
 import { LogTaskController } from './../../common/LogTask';
 import { UserService } from '../user/user.service';
-import { UserEmailNotVerified, UserPasswordError } from '../user/user.exceptions';
 import { GoogleService } from '../google/google.service';
-import { RoleProfileType, User } from '@prisma/client';
+import { User } from '@prisma/client';
 import { UserUsedGoogleAccount } from '../google/google.exceptions';
 import { SendgridService } from '../sendgrid/sendgrid.service';
-import { SENDGRID_EMAIL_TEMPLATE } from '../sendgrid/sendgrid.enum';
+import { UserPasswordError } from '../user/user.exceptions';
 
 export class AuthController {
   logTask: LogTaskController = new LogTaskController('AuthController');
@@ -31,9 +30,6 @@ export class AuthController {
     if (!(await bcrypt.compare(input.password, user.password))) {
       throw new UserPasswordError(this.server);
     }
-    if (user.emailSignupVerification.length === 0) {
-      throw new UserEmailNotVerified(this.server);
-    }
     const token = await this.authService.createJwt(user.id);
     this.logTask.end('signIn');
     reply.code(200).send({ message: token });
@@ -44,14 +40,11 @@ export class AuthController {
     const input: ISignUpInput = request.body;
 
     input.password = await bcrypt.hash(input.password, this.server.config.SALT_ROUNDS);
-    const user = await this.userService.create(
+    await this.userService.create(
       input.firstname,
       input.lastname,
       input.email,
       input.password,
-      input.newsletter,
-      input.role,
-      true,
     );
     this.logTask.end('signUp');
     reply.code(200).send({ message: true });
@@ -80,9 +73,6 @@ export class AuthController {
         googleUser.family_name,
         googleUser.email,
         null,
-        true,
-        RoleProfileType.ENTREPRENEUR,
-        false,
       );
     }
     const token = await this.authService.createJwt(user.id);

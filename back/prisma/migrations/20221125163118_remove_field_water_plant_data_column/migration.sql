37a383c6864cb7743d35f34fb4501e7a4b37bb04
cerfio <yannis.cerfio@gmail.com>
2022-11-25 18:04:14 +0100

    remove field water

---
+++ back/prisma/migrations/20221125163118_remove_field_water_plant_data_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `water` on the `PlantData` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "PlantData" DROP COLUMN "water";

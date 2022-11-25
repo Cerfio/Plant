ee12a4246e95f9090d3b7f9ea32b3b6602a738d7
cerfio <yannis.cerfio@gmail.com>
2022-11-25 18:06:19 +0100

    add data in schema plantData

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,19 +1,21 @@
 /* eslint-disable no-console */
-import * as config from "./../config/config";
-import fastify from "fastify";
-import fastifyBlipp from "fastify-blipp";
-import fastifyJwt from "@fastify/jwt";
+import * as config from './../config/config';
+import fastify from 'fastify';
+import fastifyBlipp from 'fastify-blipp';
+import fastifyJwt from '@fastify/jwt';
+import mqtt from 'mqtt';
 
-import fastifySensible from "@fastify/sensible";
-import fastifyEnv from "@fastify/env";
+import fastifySensible from '@fastify/sensible';
+import fastifyEnv from '@fastify/env';
 
 // decorates
-import jwtDecorate from "./decorates/jwt.decorate";
-import sentryDecorate from "./decorates/sentry.decorate";
+import jwtDecorate from './decorates/jwt.decorate';
+import sentryDecorate from './decorates/sentry.decorate';
 
 // routes
-import authRoutes from "./auth/auth.routes";
-import plantRoutes from "./plant/plant.routes";
+import authRoutes from './auth/auth.routes';
+import plantRoutes from './plant/plant.routes';
+import { PlantDataController } from './plantData/plantData.controller';
 
 const server: any = fastify({ logger: true });
 
@@ -23,7 +25,7 @@ const loadPlugins = async () => {
   await server.register(fastifySensible);
   await server.register(fastifyJwt, {
     secret: server.config.SECRET_JWT,
-    sign: { algorithm: "HS256", expiresIn: "31d" },
+    sign: { algorithm: 'HS256', expiresIn: '31d' },
   });
 };
 
@@ -44,7 +46,44 @@ const start = async () => {
     server.sockets = [];
     await server.ready();
     server.blipp();
-    server.listen({ port: server.config.PORT, host: "0.0.0.0" });
+    const client = mqtt.connect('mqtt://192.168.1.30', {
+      port: server.config.MQTT_PORT,
+      // username: "plant-iot",
+      // password: "58@3nv#G22zRy57dVE7&xt",
+    });
+    const topic = '/plant/data';
+    client.on('connect', () => {
+      console.log('Connected');
+      client.subscribe([topic], () => {
+        console.log(`Subscribe to topic '${topic}'`);
+      });
+    });
+
+    client.on('message', (topic, payload) => {
+      const data: {
+        serialNumber: string;
+        temperature: number;
+        humiditySoil: number;
+        humidityAir: number;
+        light: number;
+        battery: number;
+        pressure: number;
+      } = JSON.parse(payload.toString());
+
+      // create plant data
+      const plantDataController = new PlantDataController(server);
+      plantDataController.create(
+        data.serialNumber,
+        data.temperature,
+        data.humiditySoil,
+        data.humidityAir,
+        data.light,
+        data.battery,
+        data.pressure,
+      );
+    });
+
+    server.listen({ port: server.config.PORT, host: '0.0.0.0' });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
